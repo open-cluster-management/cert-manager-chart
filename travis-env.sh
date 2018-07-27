0a01e6c65ea115ecb586ba1575be9aeca6a277d2
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-07-27 09:12:36 -0400

    Testing travis build and push.

---
+++ travis-env.sh
@@ -0,0 +1,10 @@
+# Release Tag and Repository
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
+    VERSION=99.99.99
+else
+    ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
+    VERSION=0.0.0
+fi
+export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
+export VERSION="$VERSION"
