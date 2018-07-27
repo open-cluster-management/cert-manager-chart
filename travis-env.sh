cd78aadd260226fb3ebbb1351eedbff2360e42e3
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-07-27 09:53:43 -0400

    Removed the test script.

--- travis-env.sh
+++ travis-env.sh
@@ -2,9 +2,6 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
     VERSION=99.99.99
-else
-    ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
-    VERSION=0.0.0
 fi
 export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
 export VERSION_NUMBER="$VERSION"
