d900e520a02b279b68486cf0d5b01266e1d1a729
Crystal Chun <crystalchun@Crystals-MBP.attlocal.net>
2018-08-18 12:36:57 -0400

    Testing if statement fix.

--- travis-env.sh
+++ travis-env.sh
@@ -1,6 +1,6 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
+if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false"  ]] ; then
+    ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
     VERSION=99.99.99
     export VERSION_NUMBER="$VERSION"
     export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
