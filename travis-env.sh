e66b0f9e4b7fe4e11cdb02a7829ef34838d402bf
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 09:04:51 -0400

    Retesting for master branch.

--- travis-env.sh
+++ travis-env.sh
@@ -1,8 +1,11 @@
 # Release Tag and Repository
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-    ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
     VERSION=1.1.1
     
     export VERSION_NUMBER="$VERSION"
     export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
 fi
+
+echo ARTIFACTORY_REPO=$ARTIFACTORY_REPO
+echo VERSION=$VERSION_NUMBER
