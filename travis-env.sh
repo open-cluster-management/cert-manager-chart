6932c5f78de9183c4d1d8faab2a4fc2b0aeecae5
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 08:56:45 -0400

    Fixing if statement.

--- travis-env.sh
+++ travis-env.sh
@@ -1,5 +1,5 @@
 # Release Tag and Repository
-if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
     ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
     VERSION=1.1.1
     
