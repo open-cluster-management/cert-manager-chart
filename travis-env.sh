a6609bb7f770698fabb7dfd727f77270134e344c
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 10:00:52 -0400

    Fixed not in if statement.

--- travis-env.sh
+++ travis-env.sh
@@ -1,7 +1,7 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ] && [ !"$TRAVIS_PULL_REQUEST" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && ! [ "$TRAVIS_PULL_REQUEST" ]; then
     ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
-    VERSION=1.1.1
+    VERSION=99.99.99
     
     export VERSION_NUMBER="$VERSION"
     export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
