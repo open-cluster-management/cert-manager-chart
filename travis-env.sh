517580632627232c701308b9ed927e2b247c7a48
Crystal Chun <crystalchun@Crystals-MacBook-Pro.local>
2018-08-20 14:08:54 -0400

    Corrected the if statement.

--- travis-env.sh
+++ travis-env.sh
@@ -1,17 +1,10 @@
 # Release Tag and Repository
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo On Master = $TRAVIS_BRANCH
-    if ! [ "$TRAVIS_PULL_REQUEST" ]; then
+    if ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
         ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
         VERSION=99.99.99
-    
-        echo Not a pull request = $TRAVIS_PULL_REQUEST
         export VERSION_NUMBER="$VERSION"
         export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
-    fi
-
-    if ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
-        echo TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE
     fi
 fi
 
