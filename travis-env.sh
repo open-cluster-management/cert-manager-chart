245069f75872969a6818ff5576c5f52039892ed1
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 13:30:04 -0400

    Fixing if statement.

--- travis-env.sh
+++ travis-env.sh
@@ -1,10 +1,14 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ] && ! [ "$TRAVIS_PULL_REQUEST" ]; then
-    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
-    VERSION=99.99.99
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo On Master = $TRAVIS_BRANCH
+    if ! [ "$TRAVIS_PULL_REQUEST" ]; then
+        ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
+        VERSION=99.99.99
     
-    export VERSION_NUMBER="$VERSION"
-    export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
+        echo Not a pull request = $TRAVIS_PULL_REQUEST
+        export VERSION_NUMBER="$VERSION"
+        export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
+    fi
 fi
 
 echo ARTIFACTORY_REPO=$ARTIFACTORY_REPO
