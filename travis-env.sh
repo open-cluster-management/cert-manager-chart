40b095ee44959815ac10a027db3f6a9fa1af559a
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 08:30:38 -0400

    Testing out if statement with pr.

--- travis-env.sh
+++ travis-env.sh
@@ -1,5 +1,5 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     ARTIFACTORY_REPO=hyc-cloud-private-scratch-helm-local
     VERSION=99.99.99
     
