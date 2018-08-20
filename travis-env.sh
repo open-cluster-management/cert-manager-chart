f9878f3d97aeba78786579bd98f32b518183cbbc
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-08-20 09:12:42 -0400

    Testing pull request if statement.

--- travis-env.sh
+++ travis-env.sh
@@ -1,5 +1,13 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" ]; then
+
+    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
+    VERSION=0.3.2
+    
+    export VERSION_NUMBER="$VERSION"
+    export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
+
+else if [ "$TRAVIS_BRANCH" = "master" ]; then
     ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
     VERSION=1.1.1
     
