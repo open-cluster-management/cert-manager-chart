536b967e5cb4a6ea7bb3234d5b338f783166f2eb
Crystal <crystal.chun@ibm.com>
2018-10-08 20:45:32 -0400

    Added in changes and removed travis-env.sh

--- travis-env.sh
+++
@@ -1,7 +0,0 @@
-# Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
-    VERSION=99.99.99
-    export VERSION_NUMBER="$VERSION"
-    export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
-fi
