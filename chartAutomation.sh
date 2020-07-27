fdb193a6b24ecb636acdf832cc75bc6d94606f91
Connor Wyckoff <connorwyckoff@comcast.net>
2020-07-27 15:51:20 -0400

    test auto to mch-repo

---
+++ chartAutomation.sh
@@ -0,0 +1,12 @@
+# Copyright (c) 2020 Red Hat, Inc.
+
+#Script to be run in travis when charts are updated in master
+
+cd $(dirname $0)
+git clone git@github.com:open-cluster-management/multicloudhub-repo.git 
+cd multicloudhub-repo
+git checkout chartAutomation
+cicd-scripts/chart-sync.sh
+git add .
+git commit -m "[skip ci] greetings from cert-manager"
+git push  git@github.com:open-cluster-management/multicloudhub-repo.git "chartAutomation"
